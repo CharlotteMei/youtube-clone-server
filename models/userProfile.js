3eceb79143a1f53b39d36502c92491dc9db5bc1e
Yichao Wu <yichaowu0316@gmail.com>
2023-04-21 14:41:08 -0700

    modify the profile schema

--- models/userProfile.js
+++ models/userProfile.js
@@ -4,11 +4,8 @@ const userProfileSchema = new mongoose.Schema(
         username: { type: String, required: true, unique: true },
         country: { type: String, required: true },
         age: { type: String, default: "30" },
-        height: { type: String, default: "5.8" },
-        weight: { type: String, default: "80" },
-        sleep: { type: String, default: "8" },
-        water: { type: String, default: "4" },
-        heartRate: { type: String, default: "80" },
+        interest: { type: String, default: "Music" },
+        hometown: { type: String, default: "Irvine" },
         avatar: {type: String, default: "/images/avatar/profile.png"},
         banner: {type: String, default: "/images/profile-banner.jpeg"},
         likes: [
